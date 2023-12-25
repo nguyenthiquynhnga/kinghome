52b757a6a97a59eac9f87d51354b5e52bf35cd4d
ducduc222 <ducduc28052002@gmail.com>
2023-12-26 00:16:57 +0700

    fix

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,29 @@
 import styles from "./page.module.css"
 import React from "react"
 
-export default async function BlogPage(
-//     {
-//     params,
-// }: {
-//     params: { slug: string, slug_product: string }
-// }
+export async function generateStaticParams() {
+  // Fetch the list of blog posts or slugs from your API or any other source
+  // For example, assuming you have an API function fetchBlogPosts()
+//   const blogPosts = await fetchBlogPosts();
+
+//   // Return an array of objects with the necessary params
+//   return blogPosts.map((post) => ({
+//     params: {
+//       slug: post.slug,
+//     },
+//   }));
+
+return {
+    slug:'1',
+    slug_product:2
+}
+}
+
+export default async function BlogPage({
+    params,
+}: {
+    params: { slug: string, slug_product: string }
+}
 ) {
     // sử dụng cơ chế fetch data trêm server component:
     // không sử dụng useEffect, sẽ bị chậm
