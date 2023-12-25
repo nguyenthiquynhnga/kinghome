bfed9a8025c3cbf6409d439f86a38436a1cf44f6
ducduc222 <ducduc28052002@gmail.com>
2023-12-26 00:14:23 +0700

    fix

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,13 @@
 import styles from "./page.module.css"
 import React from "react"
 
-export default async function BlogPage({
-    params,
-}: {
-    params: { slug: string, slug_product: string }
-}) {
+export default async function BlogPage(
+//     {
+//     params,
+// }: {
+//     params: { slug: string, slug_product: string }
+// }
+) {
     // sử dụng cơ chế fetch data trêm server component:
     // không sử dụng useEffect, sẽ bị chậm
     // https://youtu.be/EnzPVKoz4nc?t=2275
