da689c8511a60d44b263bb5f0491fc94cc7f6b39
ducduc222 <ducduc28052002@gmail.com>
2023-12-25 23:37:48 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output:'export',
+    output:'export',
     images: {
         domains:['rcong.vn','newproductreviews.sapoapps.vn','product.hstatic.net','encrypted-tbn0.gstatic.com','bizweb.dktcdn.net','bizweb.dktcdn.net', 'gooccho.vn','noithatlacgia.vn','res.cloudinary.com']
     },
