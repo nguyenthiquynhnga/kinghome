567f7dd97e0fa9ca26bcc075fc154100817ae82d
ducduc222 <ducduc28052002@gmail.com>
2023-12-26 09:24:10 +0700

    fix

--- src/app/[slug]/[slug_product]/page.tsx
+++ src/app/[slug]/[slug_product]/page.tsx
@@ -13,12 +13,79 @@ import Review from "@/components/review-list/review"
 type Props = {
     params: { slug: string, slug_product: string }
 }
-// export async function generateStaticParams() {
-//get danh sách slug từ api
-// return posts.map((post: { slug: any; }) => ({
-//   slug: post.slug,
-// }))
-// }
+export async function generateStaticParams() {
+    // get danh sách slug từ api
+    // return posts.map((post: { slug: any; }) => ({
+    //   slug: post.slug,
+    // }))
+    return [
+      { slug: 1 },
+      { slug: 2 },
+      { slug: 3 },
+      { slug: 4 },
+      { slug: 5 },
+      { slug: 6 },
+      { slug: 7 },
+      { slug: 8 },
+      { slug: 9 },
+      { slug: 10 },
+      { slug: 11 },
+      { slug: 12 },
+      { slug: 13 },
+      { slug: 14 },
+      { slug: 15 },
+      { slug: 16 },
+      { slug: 17 },
+      { slug: 18 },
+      { slug: 19 },
+      { slug: 20 },
+      { slug: 21 },
+      { slug: 22 },
+      { slug: 23 },
+      { slug: 24 },
+      { slug: 25 },
+      { slug: 26 },
+      { slug: 27 },
+      { slug: 28 },
+      { slug: 29 },
+      { slug: 30 },
+      { slug: 31 },
+      { slug: 32 },
+      { slug: 33 },
+      { slug: 34 },
+      { slug: 35 },
+      { slug: 36 },
+      { slug: 37 },
+      { slug: 38 },
+      { slug: 39 },
+      { slug: 40 },
+      { slug: 41 },
+      { slug: 42 },
+      { slug: 43 },
+      { slug: 44 },
+      { slug: 45 },
+      { slug: 46 },
+      { slug: 47 },
+      { slug: 48 },
+      { slug: 49 },
+      { slug: 50 },
+      { slug: 51 },
+      { slug: 52 },
+      { slug: 53 },
+      { slug: 54 },
+      { slug: 55 },
+      { slug: 56 },
+      { slug: 57 },
+      { slug: 58 },
+      { slug: 59 },
+      { slug: 60 },
+      { slug: 61 },
+      { slug: 62 },
+      { slug: 63 },
+      { slug: 64 },
+      { slug: 65 },
+    ]
+  }
 export async function generateMetadata(
     { params }: Props,
     parent: ResolvingMetadata
@@ -94,7 +161,7 @@ export default async function CategoryPage({
                             </div>
                             <div className="review-list">
                                 {dataFetch.review.list_review ? dataFetch.review.list_review.map((review) => (
-                                    <Review key={review.name} review={review}/>
+                                    <Review key={review.name} review={review} />
                                 )) : 'Chưa có đánh giá nào'}
                             </div>
                         </div>
