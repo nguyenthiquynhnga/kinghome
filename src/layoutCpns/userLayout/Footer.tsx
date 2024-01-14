9cc0dfee19dcf840bae64cc7a8e717ca192ee6da
ducduc222 <ducduc28052002@gmail.com>
2024-01-14 11:17:40 +0700

    upcode

--- src/layoutCpns/userLayout/Footer.tsx
+++ src/layoutCpns/userLayout/Footer.tsx
@@ -1,9 +1,110 @@
 import React from "react";
-import "./Footer.css"
+import "./Footer.scss"
 
 const Footer = () => {
     return (
-        <div className="footer">Footer</div>
+        <div className="footer">
+            <div className="footer-content">
+                <div className="footer-box1">
+                    <div className="box1-first"><p>
+                        “Đặt sự hài lòng của khách hàng là ưu tiên số 1 trong mọi suy nghĩ hành động của mình” là sứ mệnh, là triết lý, chiến lược.. luôn cùng YODY tiến bước
+                    </p>
+                    </div>
+                    <div className="box1-last">
+                        <ul className="list-contact">
+                            <li className="list-contact__item">
+                                <a href="https://facebook.com/yody.vn" className="social-button" title="social-button" aria-label="social-button" target="_blank" rel="nofollow"><img width="40" height="40" src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/social_1.png?1703736838003" alt="social-button" /></a>
+                            </li>
+                            <li className="list-contact__item">
+                                <a href="https://www.youtube.com/channel/UCfRq5SO7a2fWyTzkGB-5t8g?view_as=subscriber" className="social-button" title="social-button" aria-label="social-button" target="_blank" rel="nofollow"><img width="40" height="40" src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/social_2.png?1703736838003" alt="social-button" /></a>
+                            </li>
+                            <li className="list-contact__item">
+                                <a href="https://www.instagram.com/yody.official/" className="social-button" title="social-button" aria-label="social-button" target="_blank" rel="nofollow"><img width="40" height="40" src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/social_3.png?1703736838003" alt="social-button" /></a>
+                            </li>
+                            <li className="list-contact__item">
+                                <a href="https://zalo.me/330373955557900892" className="social-button" title="social-button" aria-label="social-button" target="_blank" rel="nofollow"><img width="40" height="40" src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/social_4.png?1703736838003" alt="social-button" /></a>
+                            </li>
+                            <li className="list-contact__item">
+                                <a href="https://shopee.vn/yody.official" className="social-button" title="social-button" aria-label="social-button" target="_blank" rel="nofollow"><img width="40" height="40" src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/social_5.png?1703736838003" alt="social-button" /></a>
+                            </li>
+                            <li className="list-contact__item">
+                                <a href="https://www.lazada.vn/shop/yody/" className="social-button" title="social-button" aria-label="social-button" target="_blank" rel="nofollow"><img width="40" height="40" src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/social_6.png?1703736838003" alt="social-button" /></a>
+                            </li>
+                            <li className="list-contact__item">
+                                <a href="https://www.sendo.vn/thuong-hieu/yody/" className="social-button" title="social-button" aria-label="social-button" target="_blank" rel="nofollow"><img width="40" height="40" src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/social_7.png?1703736838003" alt="social-button" /></a>
+                            </li>
+                            <li className="list-contact__item">
+                                <a href="https://tiki.vn/cua-hang/yody-official-store" className="social-button" title="social-button" aria-label="social-button" target="_blank" rel="nofollow"><img width="40" height="40" src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/social_8.png?1703736838003" alt="social-button" /></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer-box2">
+                    <span className="box__title">Về KINGHOME</span>
+                    <ul className="box__list">
+                        <li className="box__list__item"><a href="/ve-kinghome">KINGHOME</a></li>
+                        <li className="box__list__item"><a href="/bao-ve-khach-hang">Bảo vệ khách hàng</a></li>
+                        <li className="box__list__item"><a href="/uu-dai-va-chinh-sach">Ưu đãi và chính sách</a></li>
+                    </ul>
+                </div>
+                <div className="footer-box3">
+                    <span className="box__title">HỖ TRỢ KHÁCH HÀNG</span>
+                    <ul className="box__list">
+                        <li className="box__list__item"><a href="/ve-kinghome">Chính sách bảo mật khách hàng thân thiết</a></li>
+                        <li className="box__list__item"><a href="/ve-kinghome">Hỗ trợ tháo lặp tại nhà</a></li>
+                        <li className="box__list__item"><a href="/ve-kinghome">Bảo hàng 1 năm nội thất</a></li>
+                        <li className="box__list__item"><a href="/ve-kinghome">Chính sách Thanh toán, giao nhận</a></li>
+                        <li className="box__list__item"><a href="/ve-kinghome">Chính sách Đồng phục</a></li>
+                        <li className="box__list__item"><a href="/ve-kinghome">Chính sách Thanh toán, giao nhận</a></li>
+                        <li className="box__list__item"><a href="/ve-kinghome">Chính sách Sử dụng Cookies</a></li>
+                    </ul>
+                </div>
+                <div className="footer-box4">
+                    <span className="box__title">CÔNG TY TNHH KINGHOME</span>
+                    <ul className="box__list">
+                        <li className="box__list__item">
+                            <p>
+                                <img src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/map.svg?1703736838003" alt="map" />
+                                <span>
+                                    Công ty TNHH KINGHOME
+                                </span>
+                            </p>
+
+                            <p>
+                                Mã số thuế: 09878787845
+                            </p>
+                            <p>
+
+                                Địa chỉ: 318-Bùi Xương Trạch-Thanh Xuân-Hà Nội
+                            </p>
+                        </li>
+                        <li className="box__list__item">
+
+                            <p>
+                                <img src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/icon_address.png?1703736838003" alt="phone" />
+
+                                <a href="/ve-kinghome">Liên hệ đặt hàng</a>
+                            </p>
+                            <p>
+                                Liên hệ sửa chữa: 0987768766
+                            </p>
+                        </li>
+                        <li className="box__list__item">
+                            <p>
+                                <img src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/email.svg?1703736838003" alt="phone" />
+                                <a href="/ve-kinghome">Email:chamsockhachhang@kinghome.vn</a>
+                            </p>
+                        </li>
+                        <li className="box__list__item--bct">
+                            <img className="lazyload loaded" src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/logo_bct.png?1703736838003" data-src="https://bizweb.dktcdn.net/100/438/408/themes/931909/assets/logo_bct.png?1703736838003" alt="bộ công thương" data-was-processed="true" />
+                            <img src="https://images.dmca.com/Badges/_dmca_premi_badge_5.png?ID=d3a2c2c5-a581-451b-b7ff-ff08fee58d6a" alt="DMCA.com Protection Status"/>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="line2"></div>
+            <div className="note">© KINGHOME - Bản quyền thuộc về Công ty TNHH KINGHOME</div>
+        </div>
     )
 }
 
